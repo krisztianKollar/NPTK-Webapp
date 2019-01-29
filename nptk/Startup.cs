bc90806e511e286090d9a863ff46faa2cba9419f
krisztianKollar <kollarkr@gmail.com>
2019-01-29 16:45:13 +0100

    Add project files.

---
+++ nptk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nptk.Startup))]
+namespace nptk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
