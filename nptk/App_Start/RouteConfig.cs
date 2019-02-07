26d89d863dd025724d9a5157a887184319f22c97
krisztianKollar <kollarkr@gmail.com>
2019-02-07 18:28:51 +0100

    New route for ApplicationUsersController

--- nptk/App_Start/RouteConfig.cs
+++ nptk/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace nptk
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               "Hikers", // Route name
+               "Hikers/{action}/{id}", // URL with parameters
+               new { controller = "ApplicationUsers", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+               );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
