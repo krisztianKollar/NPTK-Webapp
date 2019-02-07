0ce2c1c20d8db6a460d5f171904a1660b29f6f72
krisztianKollar <kollarkr@gmail.com>
2019-02-07 17:13:05 +0100

    ApplicatioUsers Contoller & views

--- nptk/Models/IdentityModels.cs
+++ nptk/Models/IdentityModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -11,17 +12,19 @@ namespace nptk.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public DateTime BirthDate { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public virtual ICollection<SignUp> SignUps { get; set; }
-
-        /*
+        [Required]
         [Display(Name = "Vezetéknév")]
-        [Display(Name = "Keresztnév")]
-        [Display(Name = "Születési dátum")]
-        [DataType(DataType.Date)]*/
+        public string LastName { get; set; }
 
+        [Required]
+        [Display(Name = "Keresztnév")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Születési idő")]
+        public DateTime BirthDate { get; set; }
+
+        public virtual ICollection<SignUp> SignUps { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -47,5 +50,7 @@ namespace nptk.Models
         public System.Data.Entity.DbSet<nptk.Models.Tour> Tours { get; set; }
 
         public System.Data.Entity.DbSet<nptk.Models.SignUp> SignUps { get; set; }
+
+        
     }
 }
