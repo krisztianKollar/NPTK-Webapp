0b6387f691c9bd455046dfb33ee11c56c2b9795f
krisztianKollar <kollarkr@gmail.com>
2019-02-28 22:10:14 +0100

    Tour model: add isactive and isactual bools to model and db.

--- nptk/Models/Tour.cs
+++ nptk/Models/Tour.cs
@@ -10,6 +10,7 @@ namespace nptk.Models
     {
 
         public int TourId { get; set; }
+
         [Display(Name = "Túra")]
         public string Title { get; set; }
 
@@ -17,12 +18,20 @@ namespace nptk.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy. MM. dd.}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
+
         [Display(Name = "Útvonal")]
         public string Track { get; set; }
+
         [Display(Name = "Távolság")]
         public decimal Distance { get; set; }
+
         [Display(Name = "Szintemelkedés")]
         public int Climb { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsActual { get; set; }
+
         [Display(Name = "Jelentkezések")]
         public virtual ICollection<SignUp> SignUps { get; set; }
 
