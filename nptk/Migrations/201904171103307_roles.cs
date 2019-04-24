87c72d9551919ec5c04049cfb2924c1f1eb9a284
krisztianKollar <kollarkr@gmail.com>
2019-04-24 11:42:01 +0200

    Minor fixings, changings.

---
+++ nptk/Migrations/201904171103307_roles.cs
@@ -0,0 +1,16 @@
+namespace nptk.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class roles : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
