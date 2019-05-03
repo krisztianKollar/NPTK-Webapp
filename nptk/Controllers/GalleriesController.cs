90f59e58daf0019b493108a48c5c42e52cb8f185
krisztianKollar <kollarkr@gmail.com>
2019-05-03 11:48:16 +0200

    Filter dropdown list items in create gallery. Update Create and change Delete + DeleteConfirmed methods.

--- nptk/Controllers/GalleriesController.cs
+++ nptk/Controllers/GalleriesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -39,7 +41,7 @@ namespace nptk.Controllers
         // GET: Galleries/Create
         public ActionResult Create()
         {
-            ViewBag.TourID = new SelectList(db.Tours, "TourId", "Title");
+            ViewBag.TourId = new SelectList(db.Tours.Where(t => t.Gallery == null), "TourId", "Title");
             return View();
         }
 
@@ -48,18 +50,66 @@ namespace nptk.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GalleryID,TourID")] Gallery gallery)
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create(GalleryViewModel model)
         {
-            if (ModelState.IsValid)
+            if (!db.Galleries.Any(g => g.TourID == model.TourId))
             {
-                db.Galleries.Add(gallery);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                Debug.WriteLine("Nincs");
             }
+            else
+            {
+                Debug.WriteLine("VAN");
+                ModelState.AddModelError("", "Ennek a túrának már van galériája!");
+            }
+            try
+            {
+                ViewBag.TourId = new SelectList(db.Tours, "TourId", "Title");
+                //var errors = ModelState.Values.SelectMany(v => v.Errors);
+                if (ModelState.IsValid)
+                {
+                    Gallery gallery = new Gallery
+                    {
+                        GalleryID = model.TourId,
+                        TourID = model.TourId
+                    };
+                    db.Galleries.Add(gallery);
+                    Debug.WriteLine("gallery.Id = " + gallery.GalleryID);
+                    db.SaveChanges();
+                    Debug.WriteLine("model.tourId = " + model.TourId);
+                    if (model.UploadedPics.Count() > 1)
+                    {
+                        var picCount = 0;
+                        foreach (var pic in model.UploadedPics)
+                        {
+                            picCount += 1;
+                            string extension = Path.GetExtension(pic.FileName);
+                            string PicPath = "tour" + model.TourId + "_" + picCount;
+                            var path = Path.Combine(Server.MapPath("/Content/TourGallery/" + PicPath + extension));
+                            pic.SaveAs(path);
+                            Picture picture = new Picture
+                            {
+                                Path = PicPath + extension,
+                                PicName = PicPath,
+                                GalleryID = gallery.GalleryID
+                            };
+                            Debug.WriteLine("picture.galleryID = " + picture.GalleryID);
+                            db.Pictures.Add(picture);
+                        }
+                    }
 
-            ViewBag.TourID = new SelectList(db.Tours, "TourId", "Title", gallery.Tour.TourId);
-            return View(gallery);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException dex)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Nem sikerült a létrehozás. Próbáld újra, s ha nem megy, keresd az adminisztrátort!");
+            }
+            return View();
         }
+
 
         // GET: Galleries/Edit/5
         public ActionResult Edit(int? id)
@@ -95,11 +145,15 @@ namespace nptk.Controllers
         }
 
         // GET: Galleries/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Sikertelen az album törlése! Próbáld újra, s ha továbbra is fennáll a probléma, keresd az adminisztrátort!";
             }
             Gallery gallery = db.Galleries.Find(id);
             if (gallery == null)
@@ -114,9 +168,26 @@ namespace nptk.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Gallery gallery = db.Galleries.Find(id);
-            db.Galleries.Remove(gallery);
-            db.SaveChanges();
+            try
+            {
+                List<Picture> picsToDelete = (from p in db.Pictures
+                                              where p.GalleryID == id
+                                              select p).ToList();
+                foreach (Picture pic in picsToDelete)
+                {
+                    var path = Path.Combine(Server.MapPath("/Content/TourGallery/" + pic.Path));
+                    System.IO.File.Delete(path);
+
+                }
+                Gallery gallery = db.Galleries.Find(id);
+                db.Galleries.Remove(gallery);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
